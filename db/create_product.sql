438b7436ebe7b263b6613f39a08a74903acc8b43
preston <mechampreston@gmail.com>
2016-08-04 00:57:29 -0600

    sql queries

---
+++ db/create_product.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (Name, Description, Price, Imageurl) values ('Beats By Dre', 'Overpriced Headphones', 299.99, 'http://www1.pcmag.com/media/images/412497-beats-by-dr-dre-studio.jpg?thumb=y');
+INSERT INTO products (Name, Description, Price, Imageurl) values ('Ergonomic Mouse', 'Comfortable Hands', 30, 'mouse.png');
+INSERT INTO products (Name, Description, Price, Imageurl) values ('Hello Kitty', 'Stuff i dont care for', 2, 'cat.png');
+INSERT INTO products (Name, Description, Price, Imageurl) values ('iPhone 23+', 'No longer a competetive device', 199, 'futurepic.png');
+INSERT INTO products (Name, Description, Price, Imageurl) values ('Blender Bottle', 'Great at keeping liquids from spilling', 12.99, 'blender.png');
